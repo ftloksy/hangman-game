e76a9571f71c8f06e5b7ca54b160047ed02006f1
Frankie Chow <freakchow@gmail.com>
2023-04-06 10:05:07 +0100

    This is the directory and comment update.

---
+++ src/pages/Footer.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+/**
+ * The footer section of the application
+ * show the title for social media link
+ * and the copyright information
+ */
+class Footer extends Component {
+
+  render() {
+    return (
+      <footer>
+        <follow-me>
+          <h2>Follow Us</h2>
+          <copyright>
+            Copyright &copy; 2023 - 2024 Frankie LLC.
+            <br/>
+            Designed By Frankie Chow
+          </copyright>
+        </follow-me>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
